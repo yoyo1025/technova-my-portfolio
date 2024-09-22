769ec595928f6c771cb095c0666f00fe6ed2866f
yoyo1025 <hsjejxys@gmail.com>
2024-09-22 16:58:56 +0900

    Aboutページ完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { InfoIcon } from "@chakra-ui/icons";
 import { Avatar, Box, Button, Center, Flex, Heading, Input, Spacer, Tab, TabList, TabPanel, TabPanels, Tabs, useColorMode, useColorModeValue, Wrap, WrapItem } from "@chakra-ui/react";
 import { Text } from '@chakra-ui/react'
 export default function Home() {
@@ -31,7 +32,21 @@ export default function Home() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <Text>one!</Text>
+            <Flex alignItems='center' gap="10px">
+              <InfoIcon boxSize={8}/>
+              <Text>私について</Text>
+            </Flex>
+            <Box bg='#dce0dd' borderRadius='5' padding='8' marginTop='5'>
+                芝浦工業大学三年生の只野陽生と申します。
+                長期やサマーインターンシップ、技術系イベント、サークルなどを通じて新たな技術を学んだり、
+                スキルを伸ばしたりしています。バックエンド・フロントエンド共に学習中ですが、チーム開発では
+                バックエンドを担当することが多いです。
+                <br/>
+                <br/>
+                普段は株式会社Donutsのミクチャエンジニアチームで長期インターンに取り組んでいます。以前は業務で
+                ミクチャの社内管理ツールのバックエンドを担当することが多かったのですが、最近はそれに加えて
+                WebフロントやDBのパフォーマンス改善を担当しています。
+            </Box>
           </TabPanel>
           <TabPanel>
             <Text>two!</Text>
