7b966e4ac29ea34b5e4ce5f9f66604893af97ac1
yoyo1025 <hsjejxys@gmail.com>
2024-09-22 16:17:01 +0900

    アバターと名前、大学名を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { Avatar, Box, Button, Center, Flex, Heading, Input, Spacer, useColorMode, useColorModeValue, Wrap, WrapItem } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-   <div>Develop</div>
-  );
+    <>
+      <Center gap='50' marginTop='10'>
+        <Box>
+          <Wrap>
+            <WrapItem>
+              <Avatar
+                name='Yosei Tadano'
+                boxSize={['100px', '150px', '170px']}
+                src="/my-face.jpg"
+              />
+            </WrapItem>
+          </Wrap>
+        </Box>
+        <Box>
+            <Text fontSize={['2xl', '3xl', '4xl']}>Yosei Tadano</Text>
+            <Text fontSize={['xs', 'xl', 'xl']}>SIT Electronics Information</Text>
+        </Box>
+      </Center>
+    </>
+  )
 }
