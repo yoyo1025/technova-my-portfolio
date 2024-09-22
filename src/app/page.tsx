8c7e7699db4b80265fa61fa63ccf26534b0dbf8c
yoyo1025 <hsjejxys@gmail.com>
2024-09-22 18:44:17 +0900

    レイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
               <InfoIcon boxSize={8}/>
               <Text>私について</Text>
             </Flex>
-            <Box bg='#dce0dd' borderRadius='5' padding='8' marginTop='5'>
+            <Box bg='#daede3' borderRadius='5' padding='8' marginTop='5'>
                 芝浦工業大学三年生の只野陽生と申します。
                 長期やサマーインターンシップ、技術系イベント、サークルなどを通じて新たな技術を学んだり、
                 スキルを伸ばしたりしています。バックエンド・フロントエンド共に学習中ですが、チーム開発では
@@ -53,23 +53,24 @@ export default function Home() {
               <AtSignIcon boxSize={8}/>
               <Text>経歴</Text>
             </Flex>
-            <Box bg='#dce0dd' borderRadius='5' padding='8' marginTop='5'>
-              <Text>2022年4月　芝浦工業大学 システム理工学部 電子情報システム学科　入学</Text>
-              <Text>2022年5月　ナビ個別指導学院　入社</Text>
-              <Text>2024年3月　ナビ個別指導学院　退社</Text>
-              <Text>2026年3月　芝浦工業大学大学院システム理工学専攻　卒業予定</Text>
+            <Box bg='#83ebc1' borderRadius='5' padding='3' marginTop='5'>
+              <Box bg="#dff7ee" padding='2' borderRadius='5' >2022年4月 <br/> 芝浦工業大学 システム理工学部 電子情報システム学科　入学</Box>
+              <Box bg="#dff7ee" padding='2' borderRadius='5' marginTop={3}>2022年5月<br/> ナビ個別指導学院　入社</Box>
+              <Box bg="#dff7ee" padding='2' borderRadius='5' marginTop={3}>2024年3月<br/> ナビ個別指導学院　退社</Box>
+              <Box bg="#dff7ee" padding='2' borderRadius='5' marginTop={3}>2026年3月<br/> 芝浦工業大学大学院システム理工学専攻　卒業予定</Box>
             </Box>
             <Flex alignItems='center' gap="10px"　 paddingTop='5'>
               <AtSignIcon boxSize={8}/>
               <Text>インターン</Text>
             </Flex>
-            <Box bg='#dce0dd' borderRadius='5' padding='8' marginTop='5'>
-              <Text>2024年2月　株式会社Donuts　（長期インターンで現在も就業中）</Text>
-              <Text>2024年7月　株式会社BuySell Technologies（サマーインターン）</Text>
-              <Text>2024年8月　合同会社DMM.com　（サマーインターン）</Text>
-              <Text>2024年8月　株式会社三菱UFJインフォメーションテクノロジー　（サマーインターン）</Text>
-              <Text>2024年8月　株式会社KDDIアジャイル　（サマーインターン）</Text>
-              <Text>2024年9月　株式会社NECソリューションイノベータ　（サマーインターン）</Text>                <Text>2024年9月　株式会社Carta Holdings　（サマーインターン）</Text>
+            <Box bg='#72cef2' borderRadius='5' padding='3' marginTop='5'>
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'>2024年2月<br/> 株式会社Donuts　（長期インターンで現在も就業中）</Box>
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5' marginTop={3}>2024年7月<br/> 株式会社BuySell Technologies（サマーインターン）</Box> 
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'marginTop={3}>2024年8月<br/> 合同会社DMM.com　（サマーインターン）</Box>
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'marginTop={3}>2024年8月<br/> 株式会社三菱UFJインフォメーションテクノロジー　（サマーインターン）</Box>
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'marginTop={3}>2024年8月<br/> 株式会社KDDIアジャイル　（サマーインターン）</Box>
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'marginTop={3}>2024年9月<br/> 株式会社NECソリューションイノベータ　（サマーインターン）</Box>                
+              <Box  bg="#dcf3fc" padding='2' borderRadius='5'marginTop={3}>2024年9月<br/> 株式会社Carta Holdings　（サマーインターン）</Box>
             </Box>
           </TabPanel>
           <TabPanel>
@@ -78,20 +79,20 @@ export default function Home() {
               <Text>使用可能技術</Text>
             </Flex>
             ・フロントエンド
-            <Box bg='#dce0dd' borderRadius='5' padding='5'>
+            <Box bg='#daede3' borderRadius='5' padding='5'>
               <Text>TypeScript / React / Next.js</Text>
             </Box>
             ・バックエンド
-            <Box bg='#dce0dd' borderRadius='5' padding='5'>
+            <Box bg='#daede3' borderRadius='5' padding='5'>
               <Text>Go / Echo</Text>
               <Text>Java / SpringBoot</Text>
             </Box>
             ・データベース
-            <Box bg='#dce0dd' borderRadius='5' padding='5'>
+            <Box bg='#daede3' borderRadius='5' padding='5'>
               <Text>RDBMS (MySQL / PostgreSQL)</Text>
             </Box>
             ・インフラ/その他
-            <Box bg='#dce0dd' borderRadius='5' padding='5'>
+            <Box bg='#daede3' borderRadius='5' padding='5'>
               <Text>Git / GitHub</Text>
               <Text>Docker</Text>
               <Text>AWS</Text>
