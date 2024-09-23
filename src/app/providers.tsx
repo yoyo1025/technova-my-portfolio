8f3d06dfe6432ffde74196d03846c1320e8173dd
yoyo1025 <hsjejxys@gmail.com>
2024-09-23 17:20:50 +0900

    不要なインポートを削除

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,12 @@
 
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-
-const theme = createTheme(); // Material UI のテーマを作成
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <CacheProvider>
             <ChakraProvider>
-                {/* <ThemeProvider theme={theme}> */}
-                    {children}
-                {/* </ThemeProvider> */}
+                {children}
             </ChakraProvider>
         </CacheProvider>
     )
